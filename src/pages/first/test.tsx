beb7afabbc9747d532c7c465b494e53181b19f8b
aqshola <aqshola.afid@gmail.com>
2025-07-20 14:54:08 +0700

    feat: :sparkles: add helmet for title

--- src/pages/first/test.tsx
+++ src/pages/first/test.tsx
@@ -1,7 +1,14 @@
-export default function Test(){
-    return(
-        <div className="[view-transition-name:main-content] z-0">
-            This is First Test Page
-        </div>
+import { Helmet } from 'react-helmet'
+
+export default function Test() {
+    return (
+        <>
+            <Helmet>
+                <title>First Page</title>
+            </Helmet>
+            <div className="[view-transition-name:main-content] z-0">
+                This is First Test Page
+            </div>
+        </>
     )
 }
--- src/pages/second/test-second.tsx
+++ src/pages/second/test-second.tsx
@@ -1,7 +1,15 @@
-export default function TestSecond(){
+import { Helmet } from "react-helmet";
+
+export default function TestSecond() {
     return (
-        <div className="[view-transition-name:main-content]">
-            This is Second Test Page
-        </div>
+        <>
+            <Helmet>
+                <title>Second Page</title>
+            </Helmet>
+            <div className="[view-transition-name:main-content]">
+                This is Second Test Page
+            </div>
+        </>
+
     )
 }
